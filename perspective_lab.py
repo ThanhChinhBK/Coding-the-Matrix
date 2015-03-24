1c46233771085ce97aee9418bbb8f0c1d18cb6df
lqxyz <liuqunxyz@gmail.com>
2015-03-24 22:32:44 +0800

    Complete perspective_lab 2015-3-24 22:32

--- perspective_lab.py
+++ perspective_lab.py
@@ -4,33 +4,31 @@ coursera = 1
 
 from vec import Vec
 from mat import Mat
-from matutil import rowdict2mat
+from matutil import rowdict2mat,coldict2mat,mat2coldict
 from solver import solve
 
 
 
 ## 1: (Task 5.12.1) Move To Board
-def move2board(y): 
+def move2board(y):
     '''
     Input:
-        - y: a Vec with domain {'y1','y2','y3'}, the coordinate representation in whiteboard coordinates of a point q
-    Output:
-        - A {'y1','y2','y3'}-Vec, the coordinate representation
-          in whiteboard coordinates of the point p such that the line through the 
-          origin and q intersects the whiteboard plane at p.
+        - y: a Vec with domain {'y1','y2','y3'}, the coordinate representation in whiteboard coordinates of a point q Output: - A {'y1','y2','y3'}-Vec, the coordinate representation in whiteboard coordinates of the point p such that the line through the origin and q intersects the whiteboard plane at p.
     '''
-    return Vec({'y1','y2','y3'}, ...)
+    return Vec({'y1','y2','y3'},{'y1':y['y1']/y['y3'], 'y2':y['y2']/y['y3'], 'y3':1 })
 
 
 
 ## 2: () Make domain of vector
 # D should be assigned the Cartesian product of R and C
-D = ...
+R={'y1','y2','y3'}
+C={'x1','x2','x3'}
+D = {(y,x) for y in R for x in C}
 
 
 
 ## 3: (Task 5.12.2) Make Equations
-def make_equations(x1, x2, w1, w2): 
+def make_equations(x1, x2, w1, w2):
     '''
     Input:
         - x1, x2: pixel coordinates of a point q in the image plane
@@ -50,7 +48,7 @@ def make_equations(x1, x2, w1, w2):
     the whiteboard, so its whiteboard coordinates are 1,0,1.  Therefore (y1/y3,y2/y3,y3/y3) = (1,0,1).
     We define w1=y1/y3 and w2=y2/y3, so w1 = 1 and w2 = 0.  Given this input-output pair, let's find
     two linear equations u*h=0 and v*h=0 constraining the unknown vector h whose entries are the entries
-    of the matrix H. 
+    of the matrix H.
 
 >>> for v in make_equations(8,25,1,0): print(v)
 <BLANKLINE>
@@ -81,22 +79,22 @@ def make_equations(x1, x2, w1, w2):
 
     Again, the negations of these vectors form an equally valid solution.
     '''
-    u = Vec(D, ...)
-    v = Vec(D, ...)
+    u = Vec(D,{('y3','x1'):w1*x1, ('y3','x2'):w1*x2, ('y3','x3'):w1,('y1','x1'):-x1, ('y1','x2'):-x2,('y1','x3'):-1})
+    v = Vec(D, {('y3','x1'):w2*x1, ('y3','x2'):w2*x2, ('y3','x3'):w2,('y2','x1'):-x1, ('y2','x2'):-x2,('y2','x3'):-1})
     return [u, v]
 
 
 
 ## 4: () Scaling row
 # This is the vector defining the scaling equation
-w = Vec(D, {...})
+w = Vec(D, {('y1','x1'):1})
 
 
 
 ## 5: () Right-hand side
 # Now construct the Vec b that serves as the right-hand side for the matrix-vector equation L*hvec=b
 # This is the {0, ..., 8}-Vec whose entries are all zero except for a 1 in position 8
-b = ...
+b = Vec({0,1,2,3,4,5,6,7,8},{8:1})
 
 
 
@@ -115,26 +113,27 @@ def make_nine_equations(corners):
     Vecs u4,u5 come from applying make_equations to the top-right corner,
     Vecs u6,u7 come from applying make_equations to the bottom-right corner,
     Vec u8 is the vector w.
-    ''' 
-    pass
-
+    '''
+    return make_equations(corners[0][0], corners[0][1],0,0)+ make_equations(corners[1][0], corners[1][1],0,1)+ make_equations(corners[2][0], corners[2][1],1,0)+ make_equations(corners[3][0], corners[3][1],1,1)+[w]
 
 
 ## 7: (Task 5.12.4) Build linear system
 # Apply make_nine_equations to the list of tuples specifying the pixel coordinates of the
 # whiteboard corners in the image.  Assign the resulting list of nine vectors to veclist:
-veclist = ...
+veclist = make_nine_equations([(358,36),(329,597),(592,157),(580,483)])
+#print(veclist)
 
 # Build a Mat whose rows are the Vecs in veclist
-L = ...
+L = rowdict2mat({i:v for (i,v) in zip(list(range(0,9)),veclist)})
 
 
 
 ## 8: () Solve linear system
 # Now solve the matrix-vector equation to get a Vec hvec, and turn it into a matrix H.
-hvec = ...
-
-H = ...
+hvec = solve(L,b)
+#print(hvec)
+H = Mat(({'y1','y2','y3'},{'x1','x2','x3'}),hvec.f)
+#print(H)
 
 
 
@@ -172,5 +171,7 @@ def mat_move2board(Y):
      y3  |     1 1    1    1
     <BLANKLINE>
     '''
-    pass
-
+    return coldict2mat([move2board(col) for col in mat2coldict(Y).values()])
+#Y_in = Mat(({'y1', 'y2', 'y3'}, {0,1,2,3}),{('y1',0):2, ('y2',0):4, ('y3',0):8,('y1',1):10, ('y2',1):5, ('y3',1):5,('y1',2):4, ('y2',2):25, ('y3',2):2,('y1',3):5, ('y2',3):10, ('y3',3):4})
+#print(Y_in)
+#print(mat_move2board(Y_in))
